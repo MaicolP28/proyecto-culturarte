6779b0b68a84bb55541f9e91fdcff094c8910a4f
Fudent <129140772+PabloVillaverde@users.noreply.github.com>
2025-08-21 00:40:42 -0300

    cu2 x2

--- src/main/java/com/culturarte/logica/datatypes/DTPropuesta.java
+++ src/main/java/com/culturarte/logica/datatypes/DTPropuesta.java
@@ -4,10 +4,48 @@
  */
 package com.culturarte.logica.datatypes;
 
+import com.culturarte.logica.clases.Categoria;
+import com.culturarte.logica.clases.Colaboracion;
+import com.culturarte.logica.clases.Colaborador;
+import com.culturarte.logica.clases.Estado;
+import com.culturarte.logica.clases.Proponente;
+import com.culturarte.logica.enums.TipoRetorno;
+import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Map;
+
 /**
  *
  * @author pablo
  */
 public class DTPropuesta {
+    private String titulo;
+    private String descripcion;
+    private String lugar;
+    private LocalDate fechaPrevista;
+    private float precioEntrada;
+    private float montoNecesario;
+    private File imagen;
+    private ArrayList<Colaboracion> colaboraciones;
+    private ArrayList<String> colaboradores;
+    private Estado estadoActual;
+    private Categoria categoria;
     
+    public DTPropuesta(){}
+    
+    public DTPropuesta(String titulo, String descripcion, String lugar, LocalDate fechaPrevista, float precioEntrada, float montoNecesario, File imagen, ArrayList<Colaboracion> colaboraciones, ArrayList<String> colaboradores, Estado estadoActual, Categoria categoria){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.lugar = lugar;
+        this.fechaPrevista = fechaPrevista;
+        this.precioEntrada = precioEntrada;
+        this.montoNecesario = montoNecesario;
+        this.imagen = imagen;
+        this.colaboraciones = colaboraciones;
+        this.colaboradores = colaboradores;
+        this.estadoActual = estadoActual;
+        this.categoria = categoria;
+    }
 }
