b70a9ba2716c5be5b937454eeebd39ab67825199
Fudent <129140772+PabloVillaverde@users.noreply.github.com>
2025-08-21 00:42:09 -0300

    cu2 IControlador

--- src/main/java/com/culturarte/logica/IControlador.java
+++ src/main/java/com/culturarte/logica/IControlador.java
@@ -6,9 +6,12 @@ package com.culturarte.logica;
 
 import com.culturarte.exepciones.UsuarioYaExiste;
 import com.culturarte.logica.datatypes.DTProponente;
+import com.culturarte.logica.datatypes.DTPropuesta;
+import com.culturarte.logica.enums.TipoEstado;
 import java.time.LocalDate;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  *
@@ -18,5 +21,6 @@ public interface IControlador {
     
     public abstract void altaColaborador(String nickname, String nombre, String apellido, String email, LocalDate fechaNacimiento, File imagen) throws UsuarioYaExiste;
     public abstract void altaProponente(String nickname, String nombre, String apellido, String email, LocalDate fechaNacimiento, File imagen, String direccion, String linkWeb, String bibliografia) throws UsuarioYaExiste;
-    public abstract ArrayList<DTProponente> listarProponentes();
+    public abstract Map<String, DTProponente> listarProponentes();
+    public abstract Map<TipoEstado, ArrayList<DTPropuesta>> listarPropuestasProponentes(String nickname);
 }
