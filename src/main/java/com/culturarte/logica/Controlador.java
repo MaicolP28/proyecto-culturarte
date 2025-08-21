fbfeb5536eca95201eea2f01876009f1dd2b3196
MaicolP28 <maicol.pastor@gmail.com>
2025-08-20 21:43:27 -0300

    cu8

--- src/main/java/com/culturarte/logica/Controlador.java
+++ src/main/java/com/culturarte/logica/Controlador.java
@@ -4,12 +4,15 @@
  */
 package com.culturarte.logica;
 
+import com.culturarte.exepciones.CategoriaYaExiste;
 import com.culturarte.exepciones.UsuarioYaExiste;
 import com.culturarte.logica.clases.*;
+import com.culturarte.logica.manejadores.ManejadorCategoria;
 import com.culturarte.logica.manejadores.ManejadorUsuario;
 import java.io.File;
 import java.time.LocalDate;
-
+import java.util.ArrayList;
+import javax.swing.tree.DefaultMutableTreeNode;
 /**
  *
  * @author maicol
@@ -40,6 +43,47 @@ public class Controlador implements IControlador{
             throw new UsuarioYaExiste("El usuario con nickname " + nickname + " ya está registrado");
         }
         mu.agregarUsuario(new Proponente(nickname, nombre, apellido, email, fechaNacimiento, imagen, direccion, linkWeb, bibliografia));
+    }
+    @Override
+    public void altaCategoria(String nombre, String catPadre) throws CategoriaYaExiste{
+        ManejadorCategoria mc = ManejadorCategoria.getInstancia();
+        
+        if (mc.buscarCategoria(nombre) == null) {
+            throw new CategoriaYaExiste("Ya existe esta categoria");
+        }
+        
+        if (catPadre == null) {
+            mc.agregarCategoriaRaiz(new Categoria(nombre, null));
+        } else {
+            Categoria padre = mc.buscarCategoria(catPadre);
+            if (padre != null) {
+                padre.addSubCategoria(new Categoria(nombre, padre));
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    @Override
+    public DefaultMutableTreeNode listarCategorias() {
+        ManejadorCategoria mc = ManejadorCategoria.getInstancia();
+        
+        DefaultMutableTreeNode raiz = new DefaultMutableTreeNode("Categorías");
+        ArrayList<Categoria> categorias = mc.getCategoriasRaiz();
+        for (Categoria categoria : categorias) {
+            raiz.add(crearNodoCategoria(categoria));
+        }
+        return raiz;
+    }
+    
+    private DefaultMutableTreeNode crearNodoCategoria(Categoria cat) {
+        DefaultMutableTreeNode nodo = new DefaultMutableTreeNode(cat);
+        for (Categoria sub : cat.getSubCategorias()) {
+            nodo.add(crearNodoCategoria(sub));
+        }
+        return nodo;
     }
     
 }
