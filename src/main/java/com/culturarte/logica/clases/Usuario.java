1b7085802c2dc84e486f866c9bc73134ced84eb1
MaicolP28 <maicol.pastor1128@gmail.com>
2025-09-09 20:30:36 -0300

    Seguir y Dejar de Seguir Testeado

--- src/main/java/com/culturarte/logica/clases/Usuario.java
+++ src/main/java/com/culturarte/logica/clases/Usuario.java
@@ -101,5 +101,18 @@ public abstract class Usuario {
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario)) return false;
+        Usuario u = (Usuario) o;
+        return nickname != null && nickname.equals(u.getNickname());
+    }
+
+    @Override
+    public int hashCode() {
+        return nickname != null ? nickname.hashCode() : 0;
+    }
 
 }
