06b005ec39a689430893f7991825d7098e02b8a4
MaicolP28 <maicol.pastor1128@gmail.com>
2025-09-09 17:06:34 -0300

    Alta categoria y Listar categoria testeados

--- src/main/java/com/culturarte/logica/clases/Categoria.java
+++ src/main/java/com/culturarte/logica/clases/Categoria.java
@@ -1,65 +1,37 @@
 package com.culturarte.logica.clases;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.io.Serializable;
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Categoria implements Serializable {
+public class Categoria {
+
     @Id
     private String nombre;
-    @ManyToOne
-    @JoinColumn(name="padre_nombre")
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "padre_nombre")
     private Categoria padre;
-    @OneToMany(mappedBy = "padre", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Categoria> subCategorias;
-    
+
+    @OneToMany(mappedBy = "padre", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Categoria> subCategorias = new ArrayList<>();
 
     public Categoria() {}
 
     public Categoria(String nombre, Categoria padre) {
-
         this.nombre = nombre;
         this.padre = padre;
-        this.subCategorias = new ArrayList<Categoria>();
-
     }
 
-    public String getNombre() {
-        return this.nombre;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
-    public Categoria getPadre() {
-        return this.padre;
-    }
-    
-    public void setPadre(Categoria padre) {
-        this.padre = padre;
-    }
+    public Categoria getPadre() { return padre; }
+    public void setPadre(Categoria padre) { this.padre = padre; }
 
-    public List<Categoria> getSubCategorias() {
-        return subCategorias;
-    }
-
-    public void addSubCategoria(Categoria subCategoria) {
-        this.subCategorias.add(subCategoria);
-    }
-    
-    public String getNombreCompleto() {
-        if (this.padre != null ) {
-            return this.padre.getNombre() + ", " + this.nombre;
-        } else {
-            return nombre;
-        }
-    }
+    public List<Categoria> getSubCategorias() { return subCategorias; }
+    public void addSubCategoria(Categoria sub) { subCategorias.add(sub); }
 }
+
+
