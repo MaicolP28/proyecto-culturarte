2ae67092e0333c5c6c03cc203b333831fb4a2f73
MaicolP28 <maicol.pastor1128@gmail.com>
2025-10-19 01:19:45 -0300

    Arreglado v2

--- src/main/java/com/culturarte/logica/AppApplication.java
+++ src/main/java/com/culturarte/logica/AppApplication.java
@@ -16,6 +16,7 @@ public class AppApplication {
 
 
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
         ConfigurableApplicationContext context = SpringApplication.run(AppApplication.class, args);
         IControlador ctrl = context.getBean(IControlador.class); // <- Aquí Spring busca el bean
 
